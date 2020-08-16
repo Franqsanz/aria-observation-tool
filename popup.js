f035537523f41aa3548db11cb5095dc7ce6b858d
franco sanchez <franco_genta@hotmail.com>
2020-08-16 14:42:33 -0300

    update popup.js

--- popup.js
+++ popup.js
@@ -1,6 +1,15 @@
 window.addEventListener('load', () => {
     console.log(`
         %cARIA DETECT RUN...`,
-        `font-size:2.5rem; background: #ffd700; text-align:left;
-    `)
+        `font-size:2.4rem;
+        background: #ffd700;
+        text-align:left;`,
+
+    )
+    console.log(`
+        %cHecho con ❤ por @Franqsanz`,
+        `font-size:1.5rem;
+        background: #000000;
+        color: #ffd700;`,
+    )
 });
