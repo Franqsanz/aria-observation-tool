096a17766d44030794363bfc6f79d3f8d3b2f478
franco sanchez <franco_genta@hotmail.com>
2020-08-18 13:46:22 -0300

    add icons

--- popup.js
+++ popup.js
@@ -2,13 +2,11 @@ window.addEventListener('load', () => {
     console.log(`
         %cARIA DETECT RUN...`,
         `font-size:2.4rem;
-        background: #ffd700;
-        text-align:left;`,
-
+        background: #ffd700;`,
     )
     console.log(`
         %cHecho con ❤ por @Franqsanz`,
-        `font-size:1.5rem;
+        `font-size:1.2rem;
         background: #000000;
         color: #ffd700;`,
     )
--- readme.md
+++ readme.md
@@ -4,4 +4,4 @@ Esta extensión detecta si se aplicó el "aria-label" en las etiquetas HTML.
 Si no se detecta serán remarcados con un borde amarillo.
 
 Ejemplo:
-![preview](preview.png)
+![preview](./icons/preview.png)
